2b9123f2d831f66679006eedc02353cadbc35f10
PhoenixMomof2 <89948037+PhoenixMomof2@users.noreply.github.com>
2021-09-24 17:50:56 -0400

    commit tree 81fb4248da3134a4b6bebb2c0d98b1b5a6b7656b

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(pickUp){
+   return Math.abs(42-pickUp);
+}
+
+function distanceFromHqInFeet(pickUp) {
+    return distanceFromHqInBlocks(pickUp)*264;
+    }
+
+function distanceTravelledInFeet(start, destination){
+    if (start < destination){
+        return (destination-start)*264
+    }else {
+        return (start-destination)*264}
+}
+
+function calculatesFarePrice(start, destination){
+    let rideDistance = (Math.abs(start-destination)*264)
+    
+    if (rideDistance <= 400) {
+        return 0
+    }
+    else if (rideDistance >400 && rideDistance<2000){
+        let farePrice = 0.02*(rideDistance-400)
+        return farePrice
+    }
+    else if (rideDistance>2000 && rideDistance<=2500){
+        return 25
+    }
+    else if (rideDistance>2500){
+        return ('cannot travel that far');
+    }
+}
+
+
